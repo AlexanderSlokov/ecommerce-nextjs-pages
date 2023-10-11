1acad8d135fcbec1d98b71730a5ec476b641bb82
AlexanderSlokov <thanhzeus2016@gmail.com>
2023-10-11 16:11:39 +0700

    [11/10/23] Connect to MongoDB and send json packet successfully
    Add own mongoose.js in lib, own connection
    
    Add Products.js in models

---
+++ models/Products.js
@@ -0,0 +1,15 @@
+import {model, Schema} from "mongoose";
+
+
+const ProductSchema = new Schema({
+    name: {type:String, required:true},
+    destination: {type: String, required: true},
+    description: {type: String, required: true},
+    price: {type: Number, required: true},
+    startDate: {type:Date, required: true},
+    endDate: {type:Date, required: true},
+    capacity: {type:Number, required: true},
+
+})
+
+export const Product = model('Product', ProductSchema);
--- pages/api/products.js
+++ pages/api/products.js
@@ -1,5 +1,18 @@
-export default function handle(req, res) {
-    res.json(req.method);
 
+import {Product} from "@/models/Products";
+import {mongooseConnect} from "@/lib/mongoose";
 
+export default async function handle(req, res) {
+   const {method} =req;
+   await mongooseConnect();
+
+   if (method === 'POST') {
+      const {name, destination, description, price, startDate, endDate, capacity } =req.body;
+
+      //async-await function
+      const productDoc = await Product.create({
+         name, destination, description, price, startDate, endDate, capacity
+      })
+      res.json(productDoc);
+   }
 }
