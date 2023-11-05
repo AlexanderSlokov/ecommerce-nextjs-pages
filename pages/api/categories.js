974b37bad7aa68382ab46a324da63c9ce7cb5859
AlexanderSlokov <thanhzeus2016@gmail.com>
2023-11-05 21:49:15 +0700

    [05/11/23]
    Categories list added.

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -1,8 +1,16 @@
 import {Category} from "@/models/Category";
+import mongoose from "mongoose";
+import {mongooseConnect} from "@/lib/mongoose";
 
 export default async function handle(req, res) {
 
     const {method} = req;
+    // Use mongoose to connect to MongoDB for GET request
+    await mongooseConnect();
+
+    if (method ==='GET') {
+        res.json(await Category.find());
+    }
 
     if (method === 'POST') {
         const {name} = req.body;
--- pages/categories.js
+++ pages/categories.js
@@ -1,14 +1,26 @@
 import Layout from "@/compoments/Layout";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import axios from "axios";
 
 export default function CategoriesPage() {
     const [name, setName] = useState('');
+    const [categories, setCategories] = useState([]);
 
+    useEffect(() => {
+        fetchCategories();
+    }, []);
+
+    //fetch categories list when load page
+    function fetchCategories () {
+        axios.get('/api/categories').then(result => {
+            setCategories(result.data);
+        });
+    }
     async function saveCategory (event) {
        event.preventDefault();
        await axios.post('api/categories', {name});
        setName('');
+       fetchCategories();
     }
 
     return (
@@ -23,6 +35,23 @@ export default function CategoriesPage() {
                        value={name}/>
                 <button type={"submit"} className={'btn-primary py-1'}>Save</button>
             </form>
+
+            <table className={"basic mt-4"}>
+                <thead>
+                <tr>
+                    <td>Category Name</td>
+                </tr>
+                </thead>
+
+                <tbody>
+                {categories.length > 0 && categories.map
+                (category => (
+                    <tr>
+                        <td>{category.name}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
 
         </Layout>
     )
