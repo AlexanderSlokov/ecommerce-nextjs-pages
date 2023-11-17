1d4f324ca9d0824f89cb017346ad18bb3f8696a8
AlexanderSlokov <thanhzeus2016@gmail.com>
2023-11-17 22:12:07 +0700

    [17/11/23]
    Add security check for admin
    
    Webpages now responsive, little annoying bugs about applying change in activelink now fixed.

--- compoments/Layout.js
+++ compoments/Layout.js
@@ -1,6 +1,7 @@
 import Nav from "@/compoments/nav";
-import { useSession, signIn, signOut } from "next-auth/react";
+import { useSession, signIn,} from "next-auth/react";
 import {useState} from "react";
+import Logo from "@/compoments/Logo";
 
 export default function Layout({children}) {
     const {data: session } = useSession();
@@ -24,6 +25,11 @@ export default function Layout({children}) {
                         <path strokeLinecap="round" strokeLinejoin="round" d="M3 4.5h14.25M3 9h9.75M3 13.5h9.75m4.5-4.5v12m0 0l-3.75-3.75M17.25 21L21 17.25" />
                     </svg>
                 </button>
+
+                <div className={"flex grow justify-center mr-6"}>
+                    <Logo/>
+                </div>
+
             </div>
 
 
