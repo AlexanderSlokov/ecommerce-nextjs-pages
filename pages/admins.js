b1caa1924dd42383773a6aad400528c453c627fe
AlexanderSlokov <thanhzeus2016@gmail.com>
2023-12-08 18:32:57 +0700

    [08/12/23] Security check for admins done.

---
+++ pages/admins.js
@@ -0,0 +1,127 @@
+import Layout from "@/components/Layout";
+import {useEffect, useState} from "react";
+import axios from "axios";
+import {withSwal} from "react-sweetalert2";
+import Spinner from "@/components/Spinner";
+import {prettyDate} from "@/lib/date";
+
+function AdminPage({swal}) {
+    const [email, setEmail] = useState('');
+    const [adminEmails, setAdminEmails] = useState([]);
+    const [isLoading, setIsLoading]=useState(false);
+    function addAdmin(ev) {
+        ev.preventDefault();
+        axios.post('/api/admins', {email}).then(r => {
+            console.log(r.data);
+            swal.fire({
+                title: 'Admin created!',
+                icon: 'success',
+            }).then(r  =>{});
+            setEmail('');
+            loadAdmins();
+        }).catch(er => {
+            swal.fire({
+                title: 'Error!',
+                text:er.response.data.message,
+                icon: 'error',
+            }).then(r  =>{});
+        });
+    }
+
+    function loadAdmins() {
+        setIsLoading(true);
+        axios.get('/api/admins').then(r => {
+            setAdminEmails(r.data);
+            setIsLoading(false);
+        })
+    }
+
+    function deleteAdmin(_id, email) {
+        swal.fire({
+            title: 'Are you sure?',
+            text: `Do you want to delete admin${email}?`,
+            showCancelButton: true,
+            cancelButtonText: 'Cancel',
+            confirmButtonText: 'Yes, Delete!',
+            confirmButtonColor: '#d55',
+            reverseButtons: true,
+        }).then(async result => {
+            if (result.isConfirmed) {
+                axios.delete('/api/admins?_id='+_id).then(() => {
+                    swal.fire({
+                        title: 'Admin deleted!',
+                        icon: 'success',
+                    }).then(r  =>{});
+                    loadAdmins();
+                })}
+        });
+    }
+
+    useEffect(() => {
+        loadAdmins();
+    }, []);
+
+    return(
+        <Layout>
+            <h1>Admins</h1>
+            <h2>Add a new admin</h2>
+
+            <form onSubmit={addAdmin}>
+                <div className={"flex gap-2"}>
+
+                    <input type="text"
+                           className={"mb-0"}
+                           placeholder={"google email"}
+                           value={email}
+                           onChange={ ev => setEmail(ev.target.value)}
+                    />
+
+                    <button
+                        type={"submit"}
+                        className={"btn-primary py-1 whitespace-nowrap"}>Add this admin</button>
+                </div>
+            </form>
+
+            <table className={"basic"}>
+                <thead>
+                <tr>
+                    <th className={"text-left"}>Admins Google Email</th>
+                    <th>Created at</th>
+                    <th></th>
+                </tr>
+                </thead>
+                {isLoading && (
+                    <tr>
+                        <td colSpan={2}>
+                            <div className={"py-4"}>
+                                <Spinner fullWidth={true}/>
+                            </div>
+                        </td>
+                    </tr>
+                )}
+                <tbody>
+                {adminEmails.length > 0 && adminEmails.map(adminEmail => (
+                    <tr key={adminEmail._id}>
+                        <td>
+                            {adminEmail.email}
+                        </td>
+                        <td>
+                            {adminEmail.createdAt && prettyDate(adminEmail.createdAt)}
+                        </td>
+
+                        <td>
+                            <button
+                                onClick={() => deleteAdmin(adminEmail._id, adminEmail.email)}
+                                className="btn-red">Delete</button>
+                        </td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </Layout>
+    );
+}
+
+export default withSwal(({swal}) => (
+    <AdminPage swal={swal}/>
+));
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,14 +5,19 @@ require('dotenv').config();
 import NextAuth, {getServerSession} from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 import clientPromise from "@/lib/mongodb";
+import {Admin} from "@/models/Admin";
 
-const adminEmails = ['thanhzeus2016@gmail.com'];
+// Function to check if an email trying to log in is an admin
+async function isAdminEmail(email) {
+    const foundAdmin = (await Admin.findOne({email}));
+    return !! foundAdmin;
+}
 
 export const authOptions = {
     DefaultAdapter: MongoDBAdapter(clientPromise),
     callbacks: {
-        session: ({session,token,user})  => {
-            if (adminEmails.includes(session?.user?.email)) {
+        session: async ({session,token,user})  => {
+            if (await isAdminEmail(session?.user?.email)) {
                 return session;
             } else {
                 return false;
@@ -32,7 +37,7 @@ export default NextAuth(authOptions);
 
 export async function isAdminRequest(req,res) {
     const session = await getServerSession(req,res,authOptions);
-    if (!adminEmails.includes(session?.user?.email)) {
+    if (!(await isAdminEmail(session?.user?.email))) {
         res.status(401);
         res.end();
         throw 'not an admin';
